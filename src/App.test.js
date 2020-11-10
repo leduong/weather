d4b8c684e9d53e016f33170cb842cbb07ddee19f
Le Duong <me@leduong.com>
2020-11-10 11:37:26 +0700

    Add demo link

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders MetaWeather Search', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/MetaWeather Search/i);
   expect(linkElement).toBeInTheDocument();
 });
